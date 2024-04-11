50bc8fcdf07ac52a2429549f1703d4858ad2105f
earnstein <bdamilola00@gmail.com>
2024-04-11 21:40:55 +0100

    chore: modified landing page

--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -1,75 +1,35 @@
-import LandingImg from "@/assets/images/LandIt.png";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import Navbar from "@/components/navbar";
 import AuroraBackground from "@/components/animated-color";
+import Product from "@/components/Product";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 
 const LandingPage = () => {
   return (
-  <>
-   <AuroraBackground>
-    <Navbar/>
-    <section id="#Landingpage" className="relative h-full w-full flex flex-1">     
-      <div className="container w-full h-full pt-16">
-        <div>
-          <div className="flex flex-col justify-center md:flex-row md:justify-between items-center gap-8">
-            {/* HERO TEXTS */}
-          <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 1 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-          className="flex flex-col gap-y-4 text-center md:text-start">
-              <h1 
-              className="text-3xl sm:text-5xl leading-tight tracking-tighter md:text-8xl lg:leading-[1.1] font-playfair text-foreground font-black">
-                Pump me: Launch Your <span>Web3</span> Journey
-              </h1>
-              <p className="max-w-2xl font-palanquin text-sm sm:text-xl text-muted-foreground tracking-tight">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, nulla?
-              </p>
-             <div className="flex gap-x-2 items-center justify-center md:justify-start">
-             <Link to="/signup">
-              <Button 
-               variant="outline"
-              className="font-palanquin sm:text-lg p-4 sm:px-20 sm:py-6 bg-background">Quick Demo</Button>
-              </Link>
+    <>
+      <AuroraBackground>
+        <Navbar />
+        <section
+          id="#Landingpage"
+          className="relative h-full w-full flex flex-1"
+        >
+          <Hero />
+        </section>
+      </AuroraBackground>
 
-              <Link to="/signup">
-              <Button 
-              className="font-palanquin sm:text-lg p-4 sm:px-20 sm:py-6">Get Started</Button>
-              </Link>
-             </div>
-            </motion.div>
-            {/*"IMAGE COMPONENT"*/}
-            <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 1 }}
-            variants={{
-              hidden: { opacity: 0, x: 50 },
-              visible: { opacity: 1, x: 0 },
-            }}
-             className="relative h-80 w-80 xs:h-[450px] xs:w-[450px] sm:h-[500px] sm:w-[600px]">
-              <img
-                src={LandingImg}
-                alt="landing"
-                className="absolute w-full h-auto inset-0 object-cover overflow-hidden"
-              />
-            </motion.div>
-          </div>
-        </div>
-      </div>
-    </section>
-    
-   </AuroraBackground>
+      <section className="w-full h-full bg-zinc-50 dark:bg-zinc-900 text-white">
+        <Product />
+      </section>
 
-  </>
+      <section className="bg-zinc-50 dark:bg-zinc-900 text-white">
+        <Features />
+      </section>
+
+      <section>
+        <Footer />
+      </section>
+    </>
   );
 };
 
